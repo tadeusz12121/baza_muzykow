e6b711c3044e392b55f7f704389e849f04918bfe
Your Name <you@example.com>
2026-08-31 20:07:37 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+const bcrypt = require("bcrypt");
+
+app.use(express.urlencoded({ extended: true }));
+
+
+
+const mongo = require("mongodb");
+const MongoClient = mongo.MongoClient;
+
+const url = "mongodb://127.0.0.1:27017";
+const client = new MongoClient(url);
+
+async function processDB() {
+    
+
+    try {
+        await client.connect();
+
+        const dbList = await client.db().admin().listDatabases();
+        const db = client.db("baza_muzykow");
+        const musicians = db.collection("musicians");
+        console.log("Databases:");
+
+
+        
+
+    } catch (err) {
+        console.error(err);
+    } finally {
+        await client.close();
+    }
+}
+
+app.post("/register", async (req, res) => {
+    await client.connect();
+    const db = client.db("baza_muzykow");
+    const musicians = db.collection("musicians");
+
+    
+    console.log(req.body);
+
+    await musicians.insertOne(req.body);
+    res.send("zarejestrowano");
+
+})
+
+app.post("/register-account", async (req, res) => {
+    console.log(req.body);
+    await client.connect();
+    const db = client.db("baza_muzykow");
+    const users = db.collection("users");
+
+    const hash = await bcrypt.hash(req.body.password, 10);
+
+    const user = {
+        username: req.body.username,
+        email: req.body.email,
+        password: hash
+    };
+    await users.insertOne(user);
+
+    res.send("konto odebrane");
+})
+
+//processDB();
+app.use(express.static("."));
+
+app.listen(3000);
